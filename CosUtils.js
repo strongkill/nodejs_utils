7eef5614d5180f95b214d7aaa761a7283674dfb8
strongkill <strongkill@yahoo.com.hk>
2018-04-08 14:56:47 +0800

    feture COS function

--- CosUtils.js
+++ CosUtils.js
@@ -1,9 +1,71 @@
 var COS = require('cos-nodejs-sdk-v5');
+var config = require('../config');
+var utils = require('./utils')
 
 var cos = new COS(config.cos);
 
 
-exports.COS_sliceUploadFile = function (file,FileDir,cb) {
+/**
+ *
+ * @param srcFile  本地文件全路径
+ * @param desFile  COS文件全路径
+ * @param cb  回调函数
+ * @private
+ */
+function _sliceUploadFile(srcFile,desFile,cb){
+    var cfg ={
+        Bucket: config.cos.bucket,
+        Region: config.cos.Region,
+        Key:desFile,
+        FilePath: srcFile
+    }
+    cos.sliceUploadFile(cfg, function (err, data) {
+        cb(err, data);
+    });
+}
+/**
+ *
+ * @param srcFile  本地文件全路径
+ * @param desFile  COS文件全路径
+ * @param cb  回调函数
+ * @private
+ */
+exports.COS_sliceUploadFile = function (srcFile,desFile,cb) {
+    _sliceUploadFileUnchangName(srcFile,desFile,cb);
+}
+
+/**
+ *  保持文件名不变。
+ * @param srcFile本地文件全路径
+ * @param desDir COS文件目录
+ * @param cb
+ * @private
+ */
+function _sliceUploadFileUnchangName (srcFile,desDir,cb){
+    var fileInfo = utils.getFilenameInfo(srcFile);
+    _sliceUploadFile(srcFile,desDir+"/"+fileInfo.fullname,cb);
+}
+
+/**
+ *  保持文件名不变。
+ * @param srcFile本地文件全路径
+ * @param desDir COS文件目录
+ * @param cb
+ * @constructor
+ */
+exports.COS_sliceUploadFileUnchangName = function (file,desDir,cb) {
+    _sliceUploadFileUnchangName(file,desDir,cb);
+}
+
+
+/*
+_sliceUploadFileUnchangName("d:\\tmp\\upload_313435d55dbdd8d6c461bf3f5bdee045.silk","/user_voice",function(err,data){
+    console.log(err);
+    console.log(data);
+})
+*/
+/*
+exports.COS_sliceUploadFile = function (file,desDir,cb) {
     var fileInfo = getFilenameInfo(file);
     var cfg ={
         Bucket: config.cos.bucket,
@@ -14,4 +76,4 @@ exports.COS_sliceUploadFile = function (file,FileDir,cb) {
     cos.sliceUploadFile(cfg, function (err, data) {
         cb(err, data);
     });
-}
+}*/
